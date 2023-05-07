f73d1504fa7575a7139154a2d9e1e0a9577fba2a
maxall41 <maxall4@protonmail.com>
2023-05-06 18:04:33 -0700

    Finished chunking

--- src/worker/sources/url.rs
+++ src/worker/sources/url.rs
@@ -22,7 +22,7 @@ use crate::worker::sources::helpers::lofty_wac_codec_to_songbird_codec;
 /// TODO: This may require some lower level work inside of Songbird/Finch
 /// TODO: This currently only supports .WAV files add support for .OGG, .MP3, .FLAC, and .AIFF
 pub async fn url_source(url: &str) -> Input {
-    let chunk_size = 2000000; // Chunk = 2MByte
+    let chunk_size = 500000; // Chunk = 500KB
     let range = HeaderValue::from_str(&format!("bytes={}-{}", 0, &chunk_size)).expect("string provided by format!");
     println!("RANGE: {:?}",range);
     let client = reqwest::Client::new();
@@ -32,8 +32,6 @@ pub async fn url_source(url: &str) -> Input {
     let bytes = resp.bytes().await.unwrap().clone();
     let metadata_bytes = bytes.clone(); // This is required because for some reason read_to_end breaks the pre-buf symph
 
-    let r = bytes.reader();
-
     metadata_bytes.reader().read_to_end(&mut pre).unwrap();
     let mut mock_file : Cursor<Vec<u8>> = Cursor::new(pre);
 
@@ -41,8 +39,6 @@ pub async fn url_source(url: &str) -> Input {
     let parsing_options = ParseOptions::new();
     let tagged_file = wav::WavFile::read_from(&mut mfp, parsing_options).unwrap();
     let properties = tagged_file.properties();
-
-    // let src = ReadOnlySource::new(r); //TODO: Fix seeking maybe see streamcatcher library
 
     let x =  Input {
         metadata: Box::new(Metadata {
@@ -59,7 +55,7 @@ pub async fn url_source(url: &str) -> Input {
             thumbnail: None,
         }),
         stereo: properties.channels() >= 2,
-        reader: Reader::StreamForURL(StreamFromURL::new(mock_file,url, chunk_size)),
+        reader: Reader::StreamForURL(StreamFromURL::new(mock_file,url, chunk_size,50000)),
         kind: lofty_wac_codec_to_songbird_codec(tagged_file.properties().format()),
         container: Container::Raw,
         pos: 0,
